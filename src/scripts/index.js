ac2e65049e24a2643a4815710de2bcf509ab5c24
saul-paulus <saulpaulus17@icloud.com>
2021-05-26 16:47:05 +0900

    memperbaiki tombol menu dan img

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -3,13 +3,25 @@ import "../styles/main.css";
 import dataRestorant from "../DATA.json";
 
 //Untuk tombol menu
-const menuTombol = document.querySelector(".menu-tombol");
+const menuTombol = document.querySelector("#menu-tombol");
 const navUl = document.querySelector("nav ul");
+const mainElement = document.querySelector("main");
+const headerEement = document.querySelector(".header");
 
-menuTombol.addEventListener("click", function () {
+menuTombol.addEventListener("click", (event) => {
   navUl.classList.toggle("open-menu");
+  event.stopProagation();
 });
 
+mainElement.addEventListener("click", (event) => {
+  navUl.classList.remove("open-menu");
+  event.stopPropagation();
+});
+
+headerEement.addEventListener("click", (event) => {
+  navUl.classList.remove("open-menu");
+  event.stopPropagation();
+});
 // Maping data restaurant
 const listPost = document.querySelector(".post");
 let listRest = "";
@@ -18,7 +30,7 @@ dataRestorant.restaurants.forEach((dataRest) => {
   listRest += `
   <article tabindex="0" id="post" class="post-item">
   <h2 class="post-item-title">${dataRest.city}</h2>
-  <img src=${dataRest.pictureId} width="450" alt="" />
+  <img src=${dataRest.pictureId} width="450" alt="${dataRest.name}" />
   <div class="post-item-description">
     <h4>Rating: ${dataRest.rating}</h4>
     <h3>${dataRest.name}</h3>
